eb43fd607631f4061d4dd9e2806bfd7d4ba9e2c5
tu <tuphitran99@gmail.com>
2018-07-02 15:09:35 -0500

    draft

--- main.cpp
+++ main.cpp
@@ -1,47 +1,95 @@
 /*
    Tu Tran
    Programming II
-   Due: June 20, 18
-   Sean Cornery Movie
-   Read data from file -> Sort movie name and star name alphabetically
+   Due: July 2, 18
+   Infix-Posfix
+   Read math formula from file -> Print out infix-postfix
  */
 
-//Read in 1 line into a string
-//Print the string
-//while there are characters left to process in the string
-//   -----
-//  | get a token (skip over blanks)
-//  | if the token is a digit then output(token)
-//  | else
-//  |    -----
-//  |   | if the token is '(' then push(token)
-//  |   | else
-//  |   |    -----
-//  |   |   | if the token is ')' then
-//  |   |   |    -----
-//  |   |   |   | while the top item is not '('
-//  |   |   |   |    pop(temp) and output(temp);
-//  |   |   |   | pop(temp)
-//  |   |   |    -----
-//  |   |   | else
-//  |   |   |    -----
-//  |   |   |   |  if the stack is empty then push(token)
-//  |   |   |   |  else
-//  |   |   |   |     -----
-//  |   |   |   |    | while the stack is not empty
-//  |   |   |   |    | and the priority (token) <= priority (top item on the stack)
-//  |   |   |   |    |     pop(temp) and output(temp)
-//  |   |   |   |    | push(token)
-//  |   |   |   |     -----
-//  |   |   |    -----
-//  |   |    ----
-//  |   -----
-//   -----
-//while the stack is not empty do pop(temp) and output(temp)
+/*if ( st[i]== *("^") ) st[i] = '3';
+   else if ( st[i]== *("*") ) st[i] = '2';
+   else if ( st[i]== *("/") ) st[i] = '2';
+   else if ( st[i]== *("+") ) st[i] = '1';
+   else if ( st[i]== *("-") ) st[i] = '1';
+   else if ( st[i]== *(")") ) st[i] = '0';
+   else if ( st[i]== *(" ") ) continue;*/
+ #include <iostream>
+ #include <fstream>
+ #include <string>
+ #include <stack>
+using namespace std;
+
+int precedence(char s){
+        if ( s == '^' ) return 3;
+        else if ( s == '*' ) return 2;
+        else if ( s == '/' ) return 2;
+        else if ( s == '+' ) return 1;
+        else if ( s == '-' ) return 1;
+        else if ( s == '(' ) return 0;
+        //else if ( s == ' ' ) continue;
+}
+int priority(char st, char top){
+        int a, b;
+        a = precedence(st);
+        b = precedence(top);
+
+        return b-a;
+
+
+};
+
+char processData(){
+        ifstream file;
+        file.open("data.txt");
+        string st;
+
+        stack <char> stk;
+        char temp;
+
+        while (getline(file, st)) {
+                for (int i = 0; i < st.length(); i++) {
+                        if ( st[i] == *(" ") ) continue;  //skip blank
+                        else{
+                                if(isdigit(st[i])) {cout << st[i];}
+                                else {
+                                        if ( st[i]== *("(") ) {stk.push(st[i]);}
+                                        else {
+                                                if ( st[i]== *(")") ) {
+                                                        while (stk.top()!= '(') {
+                                                                temp = stk.pop();
+                                                                cout << temp;
+
+                                                        }
+                                                        temp = stk.pop();
+                                                }
+                                                else {
+                                                        if (stk.empty()) {stk.push(st[i]);}
+                                                        else {
+                                                                while (!stk.empty() && priority(st[i], stk.top()) <=0) {
+                                                                        temp = stk.pop();
+                                                                        cout << temp;
+
+                                                                }
+                                                                stk.push(st[i]);
+                                                        }
+                                                }
+                                        }
+                                }
+                        }
+                }
+                while(!stk.empty()) {
+                        temp = stk.pop();
+                        cout << temp;
+
+                }
+        }
+
+        file.close();
+}
 
 
 
-//Precedence of the Operators:
 
-//  operators : ^ * / + - (
-//  precedence: 3 2 2 1 1 0
+int main(){
+        processData();
+}
