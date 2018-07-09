ee16f56920b9dd84a70dc1b911b040449215e030
tu <tuphitran99@gmail.com>
2018-07-09 11:17:09 -0500

    finish prog

--- main.cpp
+++ main.cpp
@@ -20,83 +20,84 @@
 using namespace std;
 
 int precedence(char s){
-        if ( s == '^' ) return 3;
-        else if ( s == '*' ) return 2;
-        else if ( s == '/' ) return 2;
-        else if ( s == '+' ) return 1;
-        else if ( s == '-' ) return 1;
-        else if ( s == '(' ) return 0;
-        else return 0;
+	if ( s == '^' ) return 3;
+	else if ( s == '*' ) return 2;
+	else if ( s == '/' ) return 2;
+	else if ( s == '+' ) return 1;
+	else if ( s == '-' ) return 1;
+	else if ( s == '(' ) return 0;
+	else return 0;
 };
 int priority(char st, char top){
-        int a, b;
-        a = precedence(st);
-        b = precedence(top);
+	int a, b;
+	a = precedence(st);
+	b = precedence(top);
 
-        return b-a;
+	return b-a;
 };
 
 void processData(){
-        ifstream file;
-        file.open("data.txt");
-        string st;
+	ifstream file;
+	file.open("data.txt");
+	string st;
 
 
-        char temp;
-        stack <char> stk(100);
-        while (getline(file, st)) {
+	char temp;
+	stack <char> stk(100);
+	while (getline(file, st)) {
+		cout << st <<endl;
+		for (int i = 0; i < st.length(); i++) {
+			if ( st[i] == *(" ") ) continue;                                                             //skip blank
 
-                for (int i = 0; i < st.length(); i++) {
-                        else if ( st[i] == *(" ")) continue;    //skip blank
+			else{
 
-                        else{
+				if(isdigit(st[i])) {cout << st[i];}
+				else {
+					if ( st[i]== *("(") ) {stk.push(st[i]);}
+					else {
 
-                                if(isdigit(st[i])) {cout << st[i];}
-                                else {
-                                        if ( st[i]== *("(") ) {stk.push(st[i]);}
-                                        else {
+						if ( st[i]== *(")") ) {
+							while ( stk.topVal() != '(' && !stk.isEmpty() ) {
+								stk.pop(temp);
+								cout << temp;
+								/*cout << "LINE: " << __LINE__<<endl;
+								   cout << "TOP: " << stk.items() << endl;*/
+							}
+							stk.pop(temp);
+						}
+						else {
 
-                                                if ( st[i]== *(")") ) {
-                                                        /*   while (stk.topVal()!= '(') {
-                                                                   stk.pop(temp);
-                                                                   cout << temp;
-                                                                   cout << "LINE: " << __LINE__<<endl;
-                                                                   cout << "TOP: " << stk.items() << endl;
-                                                           } */
-                                                        stk.pop(temp);
-                                                }
-                                                else {
-                                                        if (stk.isEmpty()) {stk.push(st[i]);}
-                                                        else {
-                                                                while (!stk.isEmpty() && priority(st[i], stk.topVal()) <=0) {
-                                                                        stk.pop(temp);
-                                                                        cout << temp;
-                                                                        //cout << "LINE: " << __LINE__<<endl;
+							if (stk.isEmpty()) {stk.push(st[i]);}
+							else {
+								while (!stk.isEmpty() && priority(st[i], stk.topVal()) >=0) {
+									stk.pop(temp);
+									cout << temp;
+									//cout << "LINE: " << __LINE__<<endl;
+								}
+								stk.push(st[i]);
+							}
+						}
+					}
+				}
+			}
+		}
 
-                                                                }
-                                                                stk.push(st[i]);
-                                                        }
-                                                }
-                                        }
-                                }
-                        }
-                }
+		while(!stk.isEmpty()) {
+			stk.pop(temp);
+			cout << temp;
+			//cout << "LINE: " << __LINE__<<endl;
+		}
+		cout << endl;
+	}
 
-                while(!stk.isEmpty()) {
-                        stk.pop(temp);
-                        cout << temp;
-                        //cout << "LINE: " << __LINE__<<endl;
-                }
 
-        }
-
-        file.close();
+	file.close();
 }
 
 
 
 
 int main(){
-        processData();
-        return 0;
+	processData();
+	return 0;
 }
